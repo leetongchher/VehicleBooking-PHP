256062fe843ec10ba5e827df5139908cdd3ccd04
leetongchher <tongchherly1245@gmail.com>
2023-01-07 00:51:24 +0700

    1-7-2023

---
+++ vendor/inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; <?php echo date('Y');?> Vehicle Booking System | New Version <i class ="fa fa-heartbeat"></i> By Lee Tongchher 
+       </p>
+    </div>
+    <!-- /.container -->
+  </footer>
